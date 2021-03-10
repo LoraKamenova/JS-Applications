ad5fce136745b41bfa7d5128cac8820b3f8c0177
Lora <cruzeiro@abv.bg>
2021-03-10 20:42:45 +0200

    Initial commit

---
+++ UniEnt/config/firebase.js
@@ -0,0 +1,13 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyCtKcPNXTpZOCv9gy26yMLihsn-7Rnju6g",
+  authDomain: "examprep-8a9c4.firebaseapp.com",
+  databaseURL: "https://examprep-8a9c4.firebaseio.com",
+  projectId: "examprep-8a9c4",
+  storageBucket: "examprep-8a9c4.appspot.com",
+  messagingSenderId: "143890836135",
+  appId: "1:143890836135:web:7927ac939be1bd76680982",
+  measurementId: "G-LJCFE0BL6R"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
---
+++ UniEnt/controller/user.js
@@ -0,0 +1,59 @@
+import commonPartial from './partials.js'
+import {registerUser, login, logout} from "../models/user.js";
+import {saveUserInfo, setHeader} from "./auth.js";
+
+export function getLogin(ctx) {
+    setHeader(ctx);
+    ctx.loadPartials(commonPartial).partial('./view/user/login.hbs')
+}
+
+export function getProfile(ctx) {
+    setHeader(ctx);
+    ctx.loadPartials(commonPartial).partial('./view/user/profile.hbs')
+}
+
+export function getRegister(ctx) {
+    setHeader(ctx);
+    ctx.loadPartials(commonPartial).partial('./view/user/register.hbs')
+}
+
+export function postRegister(ctx) {
+    const {username, password, rePassword} = ctx.params;
+    if (password !== rePassword) {
+        throw new Error('Passwords do not match!')
+    }
+    registerUser(username, password)
+        .then(res => {
+            saveUserInfo(res.user.email);
+            ctx.redirect('#/home');
+        }).catch(e => console.log(e));
+}
+
+export function postLogin(ctx) {
+    const {username, password} = ctx.params;
+    login(username, password)
+        .then(res => {
+            saveUserInfo(res.user.email);
+           notify('Logged in!', '#successBox')
+            setTimeout(() => {
+                ctx.redirect('#/home');
+            }, 2000)
+
+        }).catch(e => console.log(e));
+}
+
+export function getLogout(ctx) {
+    logout()
+        .then(res => {
+            sessionStorage.clear();
+            ctx.redirect('#/login');
+        }).catch(e =>{
+            notify(`${e.message}`, 'errorBox')
+    });
+}
+
+function notify(message, selector) {
+    const notification = document.querySelector(selector);
+    notification.textContent = message;
+    notification.style.display = 'block';
+}
